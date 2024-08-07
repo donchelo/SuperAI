755ae54fe9885561993fc7e55a163db72c02c947
paulo_garcia <my.name@mapanacode.com>
2024-08-07 11:01:34 -0500

    [main] Update authService to get the url from backend and then navigate to the url

--- fe/src/services/authService.ts
+++ fe/src/services/authService.ts
@@ -1,35 +1,13 @@
 import axios from 'axios';
 
+function navigateTo(url: string) {
+    window.location.href = url;
+}
+
 export const loginWithGoogle = async () => {
-
-    axios.defaults.baseURL = import.meta.env.VITE_API_URL;
-
     // Obtener URL de autenticación de Google
-    const { data } = await axios.get(`/auth/google`);
+    const { data } = await axios.post(`/auth/google`);
     console.log(data);
     // window.location.href = 'http://ai4u.com.co/app/chat';
-    
-
-    fetch(`${import.meta.env.VITE_API_URL}/auth/google`, {
-        credentials: 'include',
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/json',
-          'Access-Control-Allow-Origin': '*',
-          'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE',
-          'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
-      }
-    }).then(response => {
-        console.log(response);
-        return response.json();
-    }
-    ).then(data => {
-        console.log(data);
-        window
-        .location
-        .assign(data.url);
-    }
-    ).catch(error => {
-        console.log(error);
-    });
+    navigateTo(data.url);
 }
