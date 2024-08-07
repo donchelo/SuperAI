dc046df0a590313702a9fd3e1729294155242ff2
paulo_garcia <my.name@mapanacode.com>
2024-08-06 20:58:11 -0500

    [main] feat: Update authService to use import.meta.env for API URL

--- fe/src/services/authService.ts
+++ fe/src/services/authService.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 export const loginWithGoogle = async () => {
 
     // Obtener URL de autenticación de Google
-    const { data } = await axios.get(`{VITE_API_URL}/google`);
+    const { data } = await axios.get(`{import.meta.env.VITE_API_URL}/google`);
     console.log(data);
     // window.location.href = 'http://ai4u.com.co/app/chat';
 };
