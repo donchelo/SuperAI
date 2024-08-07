ad382a416d6dfb50bca82f6f094e27336225aa4b
paulo_garcia <my.name@mapanacode.com>
2024-08-06 23:42:02 -0500

    [main]  Update authService to use fetch for Google authentication

--- fe/src/services/authService.ts
+++ fe/src/services/authService.ts
@@ -8,7 +8,28 @@ export const loginWithGoogle = async () => {
     const { data } = await axios.get(`/auth/google`);
     console.log(data);
     // window.location.href = 'http://ai4u.com.co/app/chat';
+    
 
     fetch(`${import.meta.env.VITE_API_URL}/auth/google`, {
-        credentials: 'include'
-      })
+        credentials: 'include',
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE',
+          'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
+      }
+    }).then(response => {
+        console.log(response);
+        return response.json();
+    }
+    ).then(data => {
+        console.log(data);
+        window
+        .location
+        .assign(data.url);
+    }
+    ).catch(error => {
+        console.log(error);
+    });
+}
