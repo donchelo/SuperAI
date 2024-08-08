cbf43d2247c15209bd1f67307a5512cfe3980100
paulo_garcia <my.name@mapanacode.com>
2024-08-08 08:54:44 -0500

    [be] Update database configuration to use pg.Pool instead of Sequelize

--- be/src/config/db.js
+++ be/src/config/db.js
@@ -1,21 +1,34 @@
-import { Sequelize } from 'sequelize';
 import dotenv from 'dotenv';
-
 dotenv.config();
 
-const sequelize = new Sequelize(process.env.PG_URI, {
-  dialect: 'postgres',
-  logging: false,
-});
+import pg from 'pg';
 
-const connectDB = async () => {
-  try {
-    await sequelize.authenticate();
-    console.log('PostgreSQL connected...');
-  } catch (error) {
-    console.error('Unable to connect to the database:', error);
-    process.exit(1);
-  }
-};
+export const pool = new pg.Pool({
+  user: process.env.POSTGRES_USER,
+  host: 'localhost',
+  database: process.env.POSTGRES_DB,
+  password: process.env.POSTGRES_PASSWORD,
+  port: 5432,
+})
 
-export { sequelize, connectDB };
+// import { Sequelize } from 'sequelize';
+// import dotenv from 'dotenv';
+
+// dotenv.config();
+
+// const sequelize = new Sequelize(process.env.PG_URI, {
+//   dialect: 'postgres',
+//   logging: false,
+// });
+
+// const connectDB = async () => {
+//   try {
+//     await sequelize.authenticate();
+//     console.log('PostgreSQL connected...');
+//   } catch (error) {
+//     console.error('Unable to connect to the database:', error);
+//     process.exit(1);
+//   }
+// };
+
+// export { sequelize, connectDB };
